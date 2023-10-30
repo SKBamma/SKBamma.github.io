d6a777ba60712084ac3c7923c8558f3699bed30b
Suresh Bamma <sureshbamma321@gmail.com>
2023-10-30 13:41:41 -0500

    add

--- SD415/Assignment/w1d1/prompt.js
+++ SD415/Assignment/w1d1/prompt.js
@@ -2,3 +2,5 @@ import promptSync from "prompt-sync";
 const  prompt = promptSync ();
 let name = prompt("What is your name?")
 console.log("Hi ", name);
+let x = promp("what is your age:");
+console.log("gae is ", x);
