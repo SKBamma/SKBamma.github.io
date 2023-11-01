a0b9f7e048114f4286c04b2aec96a85f0f09c7b3
Suresh Bamma <sureshbamma321@gmail.com>
2023-11-01 09:19:31 -0500

    isPrime

--- SD415/d12function/src/app.ts
+++ SD415/d12function/src/app.ts
@@ -297,3 +297,91 @@ function printPattern4(rows: number): void {
 }
 //testing
 printPattern4(5);
+
+console.log("Testing prime Numbers")
+//Test the prime number
+function testPrime(num:number):boolean{
+    for(let i = 2; i<num; i++){
+        if(num % i === 0){
+            return false;
+        }
+    }
+    return true;
+}
+//testing primes
+console.log("true: ", testPrime(7));
+console.log("false: ", testPrime(100));
+
+console.log("Scope Example 1");
+function a() {
+    console.log(x); // consult Global for x and print 20 from Global
+}
+function b() {
+    let x = 10;
+    a(); // consult Global for a
+    console.log(x);
+}
+let x = 20;
+b();
+
+console.log("Scope Example 2");
+function c() {
+    function d() {
+        console.log(p);
+    }
+    let p = 10;
+    d();
+    console.log(p);
+}
+let p = 20;
+c(); // 10
+
+console.log("Scope Example 3");
+function e() {
+    function f() {
+        console.log(g);
+    }
+    f();
+    console.log(g);
+}
+let g = 20;
+e(); // 20
+
+console.log("Scope Example 5");
+function h() {
+    let a = 1, b = 20, c;
+    console.log(a + " " + b + " " + c); // 1 20 undefined
+    function g() {
+        let b = 300, c = 4000;
+        console.log(a + " " + b + " " + c); // 1 300 4000
+        a = a + b + c;
+        console.log(a + " " + b + " " + c); // 4301 300 4000
+    }
+    console.log(a + " " + b + " " + c); // 1 20 undefined
+    g();
+    console.log(a + " " + b + " " + c); // 4301 20 undefined
+}
+h();
+
+console.log("Scope Example 5");
+
+// let x = 10;
+// function main() {
+//     let x = 0;
+//     console.log("x1 is " + x);
+//     x = 20;
+//     console.log("x2 is " + x);
+//     if (x > 0) {
+//         x = 30;
+//         console.log("x3 is " + x);
+//     }
+//     console.log("x4 is " + x);
+//     function f(x) {
+//         console.log("x5 is " + x);
+//     }
+//     f(50);
+//     console.log("x6 is " + x);
+// }
+// main();
+// console.log("x7 is " + x);
+// //Draw the scope chain
