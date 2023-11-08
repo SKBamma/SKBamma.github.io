9b9fd5fa9fb2a48e37c5001fa25ec97b56d78401
Suresh Bamma <sureshbamma321@gmail.com>
2023-11-08 10:56:50 -0600

    w2d2 methods

---
+++ SD415/w2d2methods/src/app.ts
@@ -0,0 +1,10 @@
+let x = 5;
+let y = {x};
+console.log(y); // {x : 5}
+function sum(arr: number[]): number[] { 
+     return arr; 
+}
+const foo = {sum };
+console.log(foo);
+console.log(foo.sum);
+console.log(foo.sum([1,2,3] ));
