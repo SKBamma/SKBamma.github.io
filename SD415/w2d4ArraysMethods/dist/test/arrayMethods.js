80e644950cd2b215ba1d0936be2282e7ea388119
Suresh Bamma <sureshbamma321@gmail.com>
2023-11-14 17:38:10 -0600

    comment on chai

--- SD415/w2d4ArraysMethods/dist/test/arrayMethods.js
+++ SD415/w2d4ArraysMethods/dist/test/arrayMethods.js
@@ -1,5 +1,6 @@
-import { assert } from "chai";
+// import { assert } from "chai";
 import { doubleNumbers, doubleAge, filterAllEvenNumber, filterAgeGreaterThan10, findEvenNum } from "../src/app.js";
+import { camelize, copySorted, sortByAge, shuffle, getAverageAge } from "../src/app.js";
 let numArray = Array();
 let peopleArray = Array();
 describe("map practice", function () {
@@ -44,5 +45,54 @@ describe("filter practice", function () {
         });
         /* complete the following similar to includes even test */
         it("includes even age -- write this test");
+    });
+});
+describe('camelize', function () {
+    it('should camelize background-color', function () {
+        const result = camelize('background-color');
+        assert.strictEqual(result, 'backgroundColor');
+    });
+    it('should camelize list-style-image', function () {
+        const result = camelize('list-style-image');
+        assert.strictEqual(result, 'listStyleImage');
+    });
+});
+describe('copySorted', function () {
+    it('it should be sorted ["HTML", "JavaScript", "CSS"] without changing original array', function () {
+        const inputArray = ["HTML", "JavaScript", "CSS"];
+        const result = copySorted(inputArray);
+        assert.deepStrictEqual(result, ["CSS", "HTML", "JavaScript"]);
+        // Check that the original array is unchanged
+        assert.deepStrictEqual(inputArray, ["HTML", "JavaScript", "CSS"]);
+    });
+});
+describe("sortByAge", function () {
+    it("sorts in place by age", function () {
+        const john1 = { name: "John", age: 25, id: "1" };
+        const pete1 = { name: "Pete", age: 30, id: "2" };
+        const mary1 = { name: "Mary", age: 28, id: "3" };
+        const inputArray = [pete1, john1, mary1];
+        const output = sortByAge(inputArray);
+        assert.deepEqual(output, [john1, mary1, pete1]);
+    });
+});
+describe('shuffle', function () {
+    it('should shuffle the array', function () {
+        const arrNum = [1, 2, 3];
+        const originalArray = [...arrNum];
+        const shuffledArray = shuffle(arrNum);
+        assert.strictEqual(shuffledArray.length, originalArray.length);
+        assert.deepStrictEqual(shuffledArray.sort(), originalArray.sort());
+        assert.deepStrictEqual(arrNum, originalArray);
+    });
+});
+/* write mocha test to get average age */
+describe("getAverageAge", function () {
+    it("gets the average age", function () {
+        const john1 = { name: "John", age: 25, id: "1" };
+        const pete1 = { name: "Pete", age: 30, id: "2" };
+        const mary1 = { name: "Mary", age: 28, id: "3" };
+        const inputArray = [pete1, john1, mary1];
+        assert.equal(getAverageAge(inputArray).toFixed(3), "27.667");
     });
 });
